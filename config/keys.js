7879b066bbbfdd8f6642e3c1b945c659eceb7ca8
JP Engstrom <jpengst@gmail.com>
2018-09-09 19:11:54 -0700

    initial express server with route files

---
+++ config/keys.js
@@ -0,0 +1,3 @@
+module.exports = {
+  mongoURI: "mongodb://jpengst:goalierox1!@ds251112.mlab.com:51112/devconnect"
+}; //link to mlab database. module.exports lets us use this object available outside of this keys.js file. user/pw in link.
---
+++ server.js
@@ -0,0 +1,28 @@
+const express = require("express"); //Express is the backend framework.
+const mongoose = require("mongoose"); //Brings in mongoose
+
+const users = require("./routes/api/users");
+const profile = require("./routes/api/profile");
+const posts = require("./routes/api/posts");
+
+const app = express(); //initialize a variable to express.
+
+//DB Config
+const db = require("./config/keys").mongoURI; //database requires keys.js file, which contains our user
+
+//Connect to MongoDB through mongoose
+mongoose
+  .connect(db)
+  .then(() => console.log("MongoDB Connected")) //.then is a promise. If it connects w/out error: log connected.
+  .catch(err => console.log(err)); //.catch is to catch an error. This is also a promise.
+
+app.get("/", (req, res) => res.send("Hello world")); //Simple route to get something up and running. / is the homepage. Takes in request and response parameters.
+
+//Use Routes
+app.use("/api/users", users);
+app.use("/api/profile", profile);
+app.use("/api/posts", posts);
+
+const port = process.env.PORT || 5000; //Lets us run the server. Port 5000. process.env.PORT is for deploying to Heroku.
+
+app.listen(port, () => console.log(`Server running on port ${port}`)); //Back-tick quotes to enable string interpolation.
