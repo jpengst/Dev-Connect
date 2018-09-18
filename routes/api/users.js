7b484fd8aab0ae543b34937bb71a00567f7aa122
JP Engstrom <jpengst@gmail.com>
2018-09-17 22:11:48 -0700

    added validators

--- routes/api/users.js
+++ routes/api/users.js
@@ -6,6 +6,9 @@ const jwt = require("jsonwebtoken");
 const keys = require("../../config/keys");
 const passport = require("passport");
 
+//Load Input Validation:
+const validateRegisterInput = require("../../validation/register");
+
 // Load User model
 const User = require("../../models/User");
 
@@ -14,18 +17,25 @@ const User = require("../../models/User");
 // @access        Public route
 router.get("/test", (req, res) => res.json({ msg: "Users Works" }));
 
-// Create route for the registration page
+// Create route for the registration page:
 // @route         GET api/users/register
 // @description   Register user
 // @access        Public
 // Post request: (request, response)
 router.post("/register", (req, res) => {
+  const { errors, isValid } = validateRegisterInput(req.body);
+
+  //Check Validation
+  if (!isValid) {
+    return res.status(400).json(errors);
+  }
   //Send in our form via POST
   // Find if the email exists:
   User.findOne({ email: req.body.email }).then(user => {
     // Gets all the info in the request body
     if (user) {
-      return res.status(400).json({ email: "Email already exists" });
+      errors.email = "Email already exists";
+      return res.status(400).json(errors);
     } else {
       const avatar = gravatar.url(req.body.email, {
         // Pass in email info through gravatar
