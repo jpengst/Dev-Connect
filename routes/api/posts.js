16db16c129a9049f929e604c844ef1e42fef458c
JP Engstrom <jpengst@gmail.com>
2018-10-02 10:41:57 -0700

    added post routes

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,43 @@
 const express = require("express");
 const router = express.Router();
+const mongoose = require("mongoose");
+const passport = require("passport"); // To protect routes
+
+// Post Model
+const Post = require("../../models/Post");
+
+// Validation
+const validatePostInput = require("../../validation/post");
 
 // @route         GET api/posts/test
 // @description   Tests post route
 // @access        Public route
 router.get("/test", (req, res) => res.json({ msg: "Posts Works" }));
+
+// @route         POST api/posts/
+// @description   Create post
+// @access        Private
+router.post(
+  "/",
+  passport.authenticate("jwt", { session: false }),
+  (req, res) => {
+    const { errors, isValid } = validatePostInput(req.body);
+
+    // Check Validation
+    if (!isValid) {
+      // If any errors, send 400 with errors object
+      return res.status(400).json(errors);
+    }
+
+    const newPost = new Post({
+      text: req.body.text,
+      name: req.body.name,
+      avatar: req.body.avatar,
+      user: req.user.id // Current logged in user
+    });
+
+    newPost.save().then(post => res.json(post));
+  }
+); // Just '/' because we are already in 'api/posts'
 
 module.exports = router;
